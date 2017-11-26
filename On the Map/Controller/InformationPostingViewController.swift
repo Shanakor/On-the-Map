afa3d464e78363f71a52c9d889d0b0025942710a
shanakor <n.rammerstorfer@icloud.com>
2017-11-26 13:47:55 +0100

    Improved behaviour of "add location" scene TextFields.

--- On the Map/Controller/InformationPostingViewController.swift
+++ On the Map/Controller/InformationPostingViewController.swift
@@ -10,12 +10,26 @@ import UIKit
 
 class InformationPostingViewController: UIViewController {
 
+    // MARK: IBOutlets
+    
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var linkTextField: UITextField!
+    @IBOutlet weak var findLocationBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        enableLoginButton(false)
+        locationTextField.delegate = self
+        linkTextField.delegate = self
     }
 
+    // MARK: UI Configuration
+
+    private func enableLoginButton(_ enabled: Bool){
+        findLocationBtn.isEnabled = enabled
+        findLocationBtn.alpha = enabled ? 1.0 : 0.5
+    }
 
     // MARK: IBActions
 
@@ -23,4 +37,42 @@ class InformationPostingViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func findLocation(_ sender: Any) {
+    }
+}
+
+// MARK: Extension for UITextFieldDelegate.
+extension InformationPostingViewController : UITextFieldDelegate{
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let txtFieldText = (textField.text ?? "") as NSString
+        let textAfterUpdate = txtFieldText.replacingCharacters(in: range, with: string)
+
+        // Determine whether both textFields have text.
+        let shouldEnableLoginBtn = textAfterUpdate.lengthOfBytes(using: .utf8) != 0 &&
+                nonFirstResponderTextField().text!.lengthOfBytes(using: .utf8) != 0
+
+        self.enableLoginButton(shouldEnableLoginBtn)
+
+        return true
+    }
+
+    private func nonFirstResponderTextField() -> UITextField{
+        return locationTextField.isFirstResponder ? linkTextField : locationTextField
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == locationTextField {
+            linkTextField.becomeFirstResponder()
+            return false
+        }
+        else if textField == linkTextField{
+            if findLocationBtn.isEnabled {
+                findLocation(self)
+            }
+
+            return false
+        }
+
+        return true
+    }
 }
