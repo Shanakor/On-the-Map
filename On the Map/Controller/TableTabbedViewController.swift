8d6196807e8d8fe4a5a8003eb7eab33c70b07311
shanakor <n.rammerstorfer@icloud.com>
2017-11-26 13:03:21 +0100

    Latest 100 students are now displayed in list tab also.

--- On the Map/Controller/TableTabbedViewController.swift
+++ On the Map/Controller/TableTabbedViewController.swift
@@ -13,16 +13,51 @@ class TableTabbedViewController: BaseTabbedViewController {
     // MARK: IBOutlets
     
     @IBOutlet weak var tableView: UITableView!
+
+    // MARK: Constants
+
+    private struct Identifiers{
+        static let ReusableCell = "studentLocationCell"
+        static let TableCellImageView = "icon_pin"
+    }
     
     // MARK: Initialization.
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func studentLocationsDidLoad(success: Bool, error: ParseAPIClient.ParseAPIError?) {
-        fatalError()
+        tableView.reloadData()
+    }
+}
+
+// MARK: Extension for UITableViewDelegate and UITableViewDataSource
+extension TableTabbedViewController: UITableViewDelegate, UITableViewDataSource{
+
+    // MARK: DataSource
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return studentLocationRepository.studentLocations.count
+    }
+
+    // MARK: Delegate
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let studentLocation = studentLocationRepository.studentLocations[indexPath.row]
+
+        var cell = tableView.dequeueReusableCell(withIdentifier: Identifiers.ReusableCell)
+        if cell == nil{
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: Identifiers.ReusableCell)
+        }
+
+        cell!.imageView!.image = UIImage(named: Identifiers.TableCellImageView)
+        cell!.textLabel!.text = "\(studentLocation.firstName) \(studentLocation.lastName)"
+        cell!.detailTextLabel!.text = studentLocation.mediaURL
+
+        return cell!
     }
 }
