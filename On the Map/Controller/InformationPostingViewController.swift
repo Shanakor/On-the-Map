f78494ee98514b4beec4a9e49f91bb1010964a0f
shanakor <n.rammerstorfer@icloud.com>
2017-11-26 13:37:40 +0100

    Implemented dismissing of "add location" scene.

---
+++ On the Map/Controller/InformationPostingViewController.swift
@@ -0,0 +1,26 @@
+//
+//  InformationPostingViewController.swift
+//  On the Map
+//
+//  Created by Niklas Rammerstorfer on 26.11.17.
+//  Copyright © 2017 Niklas Rammerstorfer. All rights reserved.
+//
+
+import UIKit
+
+class InformationPostingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    // MARK: IBActions
+
+    @IBAction func dismissViewController(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
