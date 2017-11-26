596d00ea68c9cfb9fe9ed7d64ab0cf2c849049f5
shanakor <n.rammerstorfer@icloud.com>
2017-11-26 14:54:01 +0100

    Adapted UI of "add location" scene to fit all sizes.

--- On the Map/Controller/InformationPostingViewController.swift
+++ On the Map/Controller/InformationPostingViewController.swift
@@ -12,23 +12,32 @@ class InformationPostingViewController: UIViewController {
 
     // MARK: IBOutlets
     
+    @IBOutlet weak var contentScrollView: UIScrollView!
     @IBOutlet weak var locationTextField: UITextField!
     @IBOutlet weak var linkTextField: UITextField!
     @IBOutlet weak var findLocationBtn: UIButton!
-    
+
+    // MARK: Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        enableLoginButton(false)
+        enableFindLocationButton(false)
         locationTextField.delegate = self
         linkTextField.delegate = self
     }
 
-    // MARK: UI Configuration
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
-    private func enableLoginButton(_ enabled: Bool){
-        findLocationBtn.isEnabled = enabled
-        findLocationBtn.alpha = enabled ? 1.0 : 0.5
+        addKeyboardObservers()
+        locationTextField.becomeFirstResponder()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        removeKeyboardObservers()
     }
 
     // MARK: IBActions
@@ -38,6 +47,44 @@ class InformationPostingViewController: UIViewController {
     }
     
     @IBAction func findLocation(_ sender: Any) {
+    }
+}
+
+// MARK: Extension for configuring UI.
+extension InformationPostingViewController{
+
+    // MARK: Basic UI Configuration
+
+    private func enableFindLocationButton(_ enabled: Bool){
+        findLocationBtn.isEnabled = enabled
+        findLocationBtn.alpha = enabled ? 1.0 : 0.5
+    }
+
+    // MARK: Keyboard behaviour
+
+    private func addKeyboardObservers(){
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: .UIKeyboardWillHide, object: nil)
+    }
+
+    private func removeKeyboardObservers(){
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    @objc private func keyboardWillShow(_ notification: NSNotification){
+
+        // Reveal view if it is obscured by the keyboard.
+        let keyboardFrame = notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! CGRect
+        let keyboardHeight = keyboardFrame.size.height
+
+        let scrollViewInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight + 10, right: 0)
+        contentScrollView.contentInset = scrollViewInsets
+        contentScrollView.scrollIndicatorInsets = scrollViewInsets
+    }
+
+    @objc private func keyboardWillHide(_ notification: NSNotification){
+        contentScrollView.contentInset = UIEdgeInsets.zero
+        contentScrollView.scrollIndicatorInsets = UIEdgeInsets.zero
     }
 }
 
@@ -51,7 +98,7 @@ extension InformationPostingViewController : UITextFieldDelegate{
         let shouldEnableLoginBtn = textAfterUpdate.lengthOfBytes(using: .utf8) != 0 &&
                 nonFirstResponderTextField().text!.lengthOfBytes(using: .utf8) != 0
 
-        self.enableLoginButton(shouldEnableLoginBtn)
+        self.enableFindLocationButton(shouldEnableLoginBtn)
 
         return true
     }
